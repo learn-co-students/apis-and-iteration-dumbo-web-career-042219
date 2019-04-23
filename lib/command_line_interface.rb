cf32bce56f14230121cffad9c7875bb534a1a2aa
mmcgovern0 <mmcgovern.wework@gmail.com>
2019-04-23 17:18:46 -0400

    Luke I am your father

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,14 @@
+
+
+
 def welcome
   # puts out a welcome message here!
+  puts "Luke I am your father"
 end
 
 def get_character_from_user
   puts "please enter a character name"
+  character_name = gets.chomp
+  character_name.downcase
   # use gets to capture the user's input. This method should return that input, downcased.
 end
