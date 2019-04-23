95244af115e87cac217e374c005570898edafc4c
Wei <theweifeng@gmail.com>
2019-04-23 15:40:10 -0400

    Done.

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,9 @@
 def welcome
-  # puts out a welcome message here!
+  puts "Welcome to Wei and Miles' API interface - great to see you (unless you're Andrew)!"
 end
 
 def get_character_from_user
   puts "please enter a character name"
   # use gets to capture the user's input. This method should return that input, downcased.
+  gets.chomp.downcase
 end
