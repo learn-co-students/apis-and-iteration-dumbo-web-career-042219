cd8136a6f163e60f9e516f6335953788d2ccae6d
sebastosh <sebastosh@gmail.com>
2019-04-23 17:10:54 -0400

    Obiwan Kenobe provides answers

--- bin/run.rb
+++ bin/run.rb
@@ -4,5 +4,6 @@ require_relative "../lib/api_communicator.rb"
 require_relative "../lib/command_line_interface.rb"
 
 welcome
-character = get_character_from_user
-show_character_movies(character)
+character_name = get_character_from_user
+get_character_movies_from_api(character_name)
+show_character_movies(character_name)
