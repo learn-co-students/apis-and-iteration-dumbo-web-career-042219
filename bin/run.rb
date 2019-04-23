7627668128ad48f78e3c97586a2efa8e31c21f4d
askoenig <askoenig94@gmail.com>
2019-04-23 19:32:54 -0400

    First Commit

--- bin/run.rb
+++ bin/run.rb
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env ruby
 
 require_relative "../lib/api_communicator.rb"
@@ -5,4 +6,5 @@ require_relative "../lib/command_line_interface.rb"
 
 welcome
 character = get_character_from_user
+get_character_movies_from_api(character)
 show_character_movies(character)
